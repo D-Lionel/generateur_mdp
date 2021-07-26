00425be865b02201ee9ac299dc95297a3bd545fd
Lionel Danglades <danglades.lionel@gmail.com>
2021-07-26 14:57:26 +0200

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,67 @@
+﻿using FormationCS;
+using System;
+
+namespace generateur_mdp
+{
+    class Program
+    {
+
+        
+        static void Main(string[] args)
+        {
+            const int NB_MDP = 3;
+
+            int longueurmdp = outils.DemanderNombrePositifNonNul("Longueur du mdp : ");
+            int choix = outils.DemanderNombreEntre("Vous voulez un mdp avec :\n" +
+                "1 - minuscules \n" +
+                "2 - minuscules et majuscules \n" +
+                "3 - caractère et chiffres \n" +
+                "4 - Tout \n" +
+                "Votre choix : ", 1, 4);
+
+            string minuscules = "azertyuiopqsdfghjklmlwxcvbn";
+            string maj = minuscules.ToUpper();
+            string chiffres = "123456789";
+            string spec = "!@?#";
+            string alphabet = "";
+
+            if (choix == 1)
+            {
+                alphabet = minuscules;
+            }
+            else if(choix == 2)
+            {
+                alphabet = minuscules + maj;
+            }
+            else if(choix == 3)
+            {
+                alphabet = minuscules + maj + chiffres;
+            }
+            else
+            {
+                alphabet = minuscules + maj + chiffres + spec;
+            }
+
+            
+            string mdp = "";
+            int l = alphabet.Length;
+
+            
+            for (int j = 0; j<NB_MDP; j++)
+            {
+
+                for (int k = 0; k < longueurmdp; k++)
+                {
+                    Random rand = new Random();
+                    int i = rand.Next(0, l);
+                    mdp += alphabet[i];
+                }
+
+                Console.WriteLine("Mot de passe : " + mdp);
+                mdp = "";
+            }
+            
+            
+        }
+    }
+}
---
+++ outils.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FormationCS
+{
+    static class outils
+    {
+        public static int DemanderNombrePositifNonNul(string question)
+        {
+            return DemanderNombreEntre(question, 1, int.MaxValue);
+        }
+
+        public static int DemanderNombreEntre(string question, int min, int max)
+        {
+            int nombre = DemanderNombre(question);
+            if (nombre >= min && nombre <= max)
+            {
+                return nombre;
+            }
+            else
+            {
+                Console.WriteLine($"Erreur : entrez un nombre entre {min} et {max}");
+                Console.WriteLine();
+                return DemanderNombreEntre(question, min, max);
+            }
+
+        }
+
+        public static int DemanderNombre(string question)
+        {
+            int longueurInt = 0;
+            Console.Write(question);
+            while (true)
+            {
+                string longueurstr = Console.ReadLine();
+                try
+                {
+                    longueurInt = int.Parse(longueurstr);
+                    break;
+                }
+                catch
+                {
+                    Console.WriteLine("Entrez un nombre valide");
+                }
+            }
+
+
+            return longueurInt;
+        }
+    }
+}
